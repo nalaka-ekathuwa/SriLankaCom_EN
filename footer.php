ed03ce27ecf552902d21dffa547f47eeff359c91
Nalaka Dinesh <nalakadinesh123@gmail.com>
2025-04-01 18:31:39 +0300

    Footer Tranlation

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
         <div class="container">
             <div class="insta-inner">
                 <div class="follow-button"> <a target="_blank" href="<?php echo $socialmedia['instagram']; ?>">
-                        <h5 class="m-0 rounded"><i class="fab fa-instagram"></i> Подписывайтесь на Instagram</h5>
+                        <!-- <h5 class="m-0 rounded"><i class="fab fa-instagram"></i> Подписывайтесь на Instagram</h5> -->
                     </a>
-                    <!--<h5 class="m-0 rounded"><i class="fab fa-instagram"></i> Follow on Instagram</h5>-->
+                    <h5 class="m-0 rounded"><i class="fab fa-instagram"></i> Follow on Instagram</h5>
 
                 </div>
                 <div class="row attract-slider">
@@ -32,18 +32,19 @@
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4 pe-4">
                     <div class="footer-about">
                         <img src="images/logo-white.png" alt="">
-                        <p class="mt-3 mb-3 white">
+                        <!-- <p class="mt-3 mb-3 white">
                             Исследуйте удивительные уголки Шри-Ланки вместе с нами. Мы организуем незабываемые туры,
                             которые подарят вам яркие впечатления и уникальные эмоции.
+                        </p> -->
+                        <p class="mt-3 mb-3 white">
+                            Explore the amazing corners of Sri Lanka with us. We organize unforgettable tours that will
+                            give you vivid impressions and unique emotions.
                         </p>
-                        <!--<p class="mt-3 mb-3 white">
-    Explore the amazing corners of Sri Lanka with us. We organize unforgettable tours that will give you vivid impressions and unique emotions.
-</p>-->
 
                         <ul>
                             <li class="white"><strong>PO Box:</strong> +39-344-685-7143</li>
-                            <li class="white"><strong>Местоположение:</strong> Патхегама, Балапития, Шри-Ланка</li>
-                            <!--<li class="white"><strong>Location:</strong> Pathegama, Balapitiya, Sri Lanka</li>-->
+                            <!-- <li class="white"><strong>Местоположение:</strong> Патхегама, Балапития, Шри-Ланка</li> -->
+                            <li class="white"><strong>Location:</strong> Pathegama, Balapitiya, Sri Lanka</li>
 
                             <li class="white"><strong>Email:</strong> <a href="/cdn-cgi/l/email-protection"
                                     class="__cf_email__"
@@ -54,38 +55,38 @@
                 </div>
                 <div class="col-lg-2 col-md-6 col-sm-12 mb-4">
                     <div class="footer-links">
-                        <h3 class="white">Быстрые ссылки</h3>
+                        <h3 class="white">Quick Links</h3>
                         <ul>
-                            <li><a href="index.php">Главная</a></li>
-                            <li><a href="about.php">О нас</a></li>
-                            <li><a href="destinations.php">Направления</a></li>
-                            <li><a href="tuors.php">Туры</a></li>
-                            <li><a href="contacts.php">Контакты</a></li>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="destinations.php">Destinations</a></li>
+                            <li><a href="tuors.php">Tours</a></li>
+                            <li><a href="contacts.php">Contacts</a></li>
                             <!-- <li><a href="##">Return Policy</a></li> -->
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-6 col-sm-12 mb-4">
                     <div class="footer-links">
-                        <h3 class="white">направления</h3>
+                        <h3 class="white">directions</h3>
                         <ul>
-                            <li><a href="destinations.php">ЭЛЛА</a></li>
-                            <li><a href="destinations.php">НУВАРА ЭЛИЯ</a></li>
-                            <li><a href="destinations.php">КАНДИ</a></li>
-                            <li><a href="destinations.php">Галле</a></li>
-                            <li><a href="destinations.php">Хортон Плейнс</a></li>
-                            <li><a href="destinations.php">Аругамбай</a></li>
+                            <li><a href="destinations.php">ELLA</a></li>
+                            <li><a href="destinations.php">NUWARA ELIA</a></li>
+                            <li><a href="destinations.php">KANDY</a></li>
+                            <li><a href="destinations.php">Galle</a></li>
+                            <li><a href="destinations.php">Horton Plains</a></li>
+                            <li><a href="destinations.php">Arugambay</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                     <div class="footer-links">
-                        <h3 class="white">Посетите нас</h3>
+                        <h3 class="white">Visit us</h3>
                         <div class="newsletter-form ">
                             <iframe height="260"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6250.255910534036!2d80.0383488115832!3d6.270782440728704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae465955bc09a25%3A0xbdfadcdadec487fb!2sElla!5e0!3m2!1sen!2slk!4v1738512823920!5m2!1sen!2slk"></iframe>
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6250.255910534036!2d80.0383488115832!3d6.270782440728704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae465955bc09a25%3A0xbdfadcdadec487fb!2sElla!5e0!3m2!1sen!2slk!4v1738512823920!5m2!1sen!2slk"></iframe>
 
-                               </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -135,8 +136,8 @@
         <div class="container">
             <div class="copyright-inner rounded p-3 d-md-flex align-items-center justify-content-between">
                 <div class="copyright-text">
-                    <p class="m-0 white"><?php echo date("Y"); ?><span style="color:#fdc703 "> Все права
-                            защищены.</span> Разработано и создано <a href="https://promodesoftware.com/"
+                    <p class="m-0 white"><?php echo date("Y"); ?><span style="color:#fdc703 "> All rights
+                            reserved.</span> Designed and created by <a href="https://promodesoftware.com/"
                             style="color:#20c997">Promode software solutions</a>.</p>
                 </div>
                 <div class="social-links">
@@ -165,11 +166,13 @@
 <!-- Back to top ends -->
 
 <!-- WhatsApp Button -->
-<a href="<?php echo $socialmedia['whatsapp']; ?>" target="_blank" class="whatsapp-button" title="Chat with us on WhatsApp">
+<a href="<?php echo $socialmedia['whatsapp']; ?>" target="_blank" class="whatsapp-button"
+    title="Chat with us on WhatsApp">
     <img src="images/whatsapp.png" alt="WhatsApp Chat">
 </a>
 
 <!-- Telegram Button -->
-<a href="<?php echo $socialmedia['telegram']; ?>" target="_blank" class="telegram-button" title="Chat with us on Telegram">
+<a href="<?php echo $socialmedia['telegram']; ?>" target="_blank" class="telegram-button"
+    title="Chat with us on Telegram">
     <img src="images/telegram.svg" alt="Telegram Chat">
 </a>
